9f7f51d0c86994750483e09f65d4546009413d98
codecommiter-at-183233357101 <marcos.borges@riachuelo.com.br>
2021-10-25 04:10:06 -0300

    ....

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,19 +1,3 @@
-terraform {
-    #backend "s3" {}
-}
-
-provider "aws" {
-    region = var.region
-    dynamic "assume_role" {
-        for_each = var.assume_role_enable ? [true] : []
-        content {
-            role_arn    =  var.assume_role_arn
-            external_id =  var.assume_role_external_id
-        }
-    }
-}
-
-
 data "aws_vpcs" "current" {
     filter {
         name   = "tag:Name"
@@ -38,7 +22,7 @@ data "aws_vpcs" "current" {
 }
 
 
-data "aws_ami" "amazon-linux-2" {
+data "aws_ami" "amazon_linux_2" {
     most_recent = true
     filter {
         name   = "owner-alias"
@@ -53,31 +37,75 @@ data "aws_ami" "amazon-linux-2" {
 
 module "loadtest" {
     source = "../../"
-    name = "nome-da-implantacao"
-    loadtest_dir_source = "./assets"
-    loadtest_dir_destination = "/loadtest"
+
     vpc_id = data.aws_vpcs.current.id
     subnet_id = data.aws_subnets.current.id
-    leader_ami_id = ""
+
+    name = "nome-da-implantacao"
+
+    executor = "jmeter"
+    loadtest_dir_source = "./assets"
+    loadtest_dir_destination = "/loadtest"
+    loadtest_entrypoint = "bzt -q -o execution.0.distributed=\"{NODES_IPS}\" *.yml"
+    
     leader_instance_type = "t2.medium"
+    setup_instance_leader_jmeter_opts = " -Xms12g -Xmx80g -XX:MaxMetaspaceSize=512m -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:G1ReservePercent=20 "
+    leader_ami_id = data.aws_ami.amazon_linux_2.id
+
     leader_tags = {
         "Name" = "nome-da-implantacao-leader",
         "Owner": "nome-do-proprietario",
         "Environment": "producao",
         "Role": "leader"
     }
+
     nodes_total = 3
-    nodes_ami_id = ""
+    nodes_ami_id = data.aws_ami.amazon_linux_2.id
     nodes_intance_type = "t2.medium"
+    setup_instance_nodes_jmeter_opts = " -Xms12g -Xmx80g -XX:MaxMetaspaceSize=512m -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:G1ReservePercent=20 "
+ 
     nodes_tags = {
         "Name": "nome-da-implantacao",
         "Owner": "nome-do-proprietario",
         "Environment": "producao",
         "Role": "node"
     }
+    
     tags = {
         "Name": "nome-da-implantacao",
         "Owner": "nome-do-proprietario",
         "Environment": "producao"
     }
 }
+
+variable "assume_role_enable" {
+    default = false
+    type = bool
+}
+
+variable "assume_role_arn" {
+    default = ""
+    type = string
+}
+
+variable "assume_role_external_id" {
+    default = ""
+    type = string
+}
+
+terraform {
+    #backend "s3" {}
+}
+
+provider "aws" {
+    region = var.region
+    dynamic "assume_role" {
+        for_each = var.assume_role_enable ? [true] : []
+        content {
+            role_arn    =  var.assume_role_arn
+            external_id =  var.assume_role_external_id
+        }
+    }
+}
+
+
---
+++ examples/plan/locust/script.py
@@ -0,0 +1,9 @@
+import time
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/")
